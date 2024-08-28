f536990d431315ba918eb08975509eeff43dfeb8
Edanriell <Edanriell@gmail.com>
2024-08-28 20:23:20 +0300

    finished mobile version

--- Client/src/widgets/layouts/main/ui/main-layout.tsx
+++ Client/src/widgets/layouts/main/ui/main-layout.tsx
@@ -3,6 +3,8 @@ import { FC, Fragment, ReactNode } from "react";
 import { Header } from "@widgets/header/ui";
 import { Footer } from "@widgets/footer/ui";
 
+import { StripeLeftMobile, StripeRightMobile } from "@widgets/layouts/main/ui/styles.ts";
+
 type MainLayoutProps = {
 	children: ReactNode;
 };
@@ -10,6 +12,8 @@ type MainLayoutProps = {
 export const MainLayout: FC<MainLayoutProps> = ({ children }) => {
 	return (
 		<Fragment>
+			<StripeRightMobile src="/images/vector/figures/stripe-right-mobile.svg" alt="" />
+			<StripeLeftMobile src="/images/vector/figures/stripe-left-mobile.svg" alt="" />
 			<Header />
 			{children}
 			<Footer />
